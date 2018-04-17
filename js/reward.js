b6d908269dcdeb12ab49900b2f540a85a313017c
Javaer <zwd_doit@163.com>
2018-04-17 11:40:52 +0800

    Site updated: 2018-04-17 11:40:52

--- js/reward.js
+++ js/reward.js
@@ -4,7 +4,6 @@
 
     rewardCheck.click(function () {
         $(this).addClass('checked').siblings(rewardCheck).removeClass('checked');
-        alert($(this).attr('data-id'))
         rewardCode.attr('src', $(this).attr('data-id') === 'wechat' ? this.dataset.wechat : this.dataset.alipay);
     });
 })(jQuery);
