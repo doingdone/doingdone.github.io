9f058e4613195d7fa890e2fffa6e117e1f5b9b76
Javaer <zwd_doit@163.com>
2018-04-17 11:54:19 +0800

    Site updated: 2018-04-17 11:54:19

--- js/reward.js
+++ js/reward.js
@@ -5,6 +5,7 @@
     rewardCheck.click(function () {
         $(this).addClass('checked').siblings(rewardCheck).removeClass('checked');
         alert($(this).attr('data-id') === 'wechat')
+        alert(rewardCode.length)
         rewardCode.attr('src', $(this).attr('data-id') === 'wechat' ? this.dataset.wechat : this.dataset.alipay);
         alert(rewardCode.attr('src'))
     });
